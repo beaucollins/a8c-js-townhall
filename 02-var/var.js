363f1f150aa944238270586cc696e8d2b518d5f2
Beau Collins <beaucollins@gmail.com>
2013-01-17 18:43:46 -0800

    more info on var and scoping

--- 02-var/var.js
+++ 02-var/var.js
@@ -1,7 +1,12 @@
+// var declares that these variables are owned by the current
+// context.
 var outside = 'outside', leaked, inside;
 
 function leaky(){
+	// inside is now only available inside the leaky function
 	var inside = "secret";
+	// we didn't declare a var here so it's going to be available outside
+	// of this function
 	leaked = "leaked";
 	console.log("Inside leaky", inside, leaked);
 }
@@ -9,7 +14,7 @@ function leaky(){
 leaky();
 // > Inside leaky secret leaked
 
-// our "inside" wasn't changed but "leaked" was
+// our "inside" wasn't changed but "leaked" was be
 console.log("Outside leaky", inside, leaked);
 // > Outside leaky, undefined, leaked
 
@@ -28,3 +33,34 @@ try {
 	console.error(e.message);
 }
 // > Can't find variable: myObject
+
+// functions can be nested and var will make sure a variable remains
+// in it's context
+
+var scope1 = function(){
+	var one = 'a', two = 'b', three = 'c';
+	var scope2 = function(){
+		var two = '2', three = '3';
+		// override one which is defined in scope1
+		var scope3 = function(){
+			var three = "LOL";
+			// override one, which is originally defined in scope1
+			one = 'LOL';
+			// override two, which is defined in scope2, but two in scope1 is
+			// not changed here
+			two = 'LOL';
+			console.log( one, two, three );
+			// > LOL LOL LOL
+		}
+		scope3();
+		console.log( one, two, three );
+		// > LOL LOL 3
+	}
+	scope2();
+	// one was overridden in scope3, but two and three were redeclared in
+	// scope2 and scope3 so they weren't modified
+	console.log(one, two, three)
+	// > LOL b c
+}
+scope1();
+
